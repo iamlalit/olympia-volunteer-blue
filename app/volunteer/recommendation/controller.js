d4bddcf880195deb603b0da746cb2ede4e890b8d
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-09-28 22:53:52 +0530

    updated for recommendation page

--- app/volunteer/recommendation/controller.js
+++ app/volunteer/recommendation/controller.js
@@ -2,4 +2,22 @@
 landingController.controller('landingPage', ['$scope', function ($scope) {
 
 
+//ask for recommendation dummy data
+$scope.schools = [];
+$scope.jobs = [];
+$scope.schools.push({'name': "University of Amsterdam",
+					'degree': "BSC",
+					'areaOfStudy': 'Science',
+					'startDate': '2004',
+					'endDate': '2007'
+					});
+$scope.jobs.push({'name': 'Red Cross NL',
+				'city': 'Amsterdam',
+				'country': 'Netherlands',
+				'title': "Volunteer",
+				'month1': '1',
+				'startDate': '2010',
+				'month2': '9',
+				'endDate': '2014'});
+
 }]);
