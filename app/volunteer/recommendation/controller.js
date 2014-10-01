77c0ea8cce38b4c14c610b44792ec9e3c213bbdb
harsh.lamba <harsh.lamba@3pillarglobal.com>
2014-10-02 00:57:20 +0530

    recommendation

--- app/volunteer/recommendation/controller.js
+++ app/volunteer/recommendation/controller.js
@@ -1,6 +1,7 @@
 ﻿var landingController = angular.module('recommendationController', []);
 landingController.controller('recommendationPage', ['$scope', function ($scope) {
     $scope.showMode = true;$scope.editMode=false;
+    $scope.showMode1 = true;$scope.editMode1=false;
     $scope.displayText2 = "Hi Marleen Bosch, " +
      "Here's a revised version of  the recommendation I wrote for you.  Let me know if you need anything else";
     $scope.updateValue =  function(){
@@ -13,5 +14,18 @@ landingController.controller('recommendationPage', ['$scope', function ($scope) 
     $scope.editValue =  function(){
         $scope.showMode = true;
         $scope.editMode = false;
+    }
+    $scope.displayText3 = "Hi Geertruda Brouwer, " +
+     "Here's a revised version of  the recommendation I wrote for you.  Let me know if you need anything else";
+    $scope.updateValue1 =  function(){
+        $scope.showMode1 = false;
+        $scope.editMode1 = true;
+        var paraOne = $('p.paraOne').text();
+        console.log(paraOne);
+        $scope.displayText = $.trim(paraOne);
+    }
+    $scope.editValue1 =  function(){
+        $scope.showMode1 = true;
+        $scope.editMode1 = false;
     }
 }]);
