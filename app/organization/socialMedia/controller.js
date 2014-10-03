d4ba5c5cb5ab58bb88451b15904f160e43f18f8c
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-10-03 23:50:02 +0530

    updated for change passwornd and updated link for social media and socila media page

---
+++ app/organization/socialMedia/controller.js
@@ -0,0 +1,15 @@
+'use strict';
+/* main App */
+var app = angular.module('socialMediaController', []);
+
+app.controller('socialMediaCtrl', function($scope){
+
+    $scope.typeOfOwner = '';
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    $scope.typeOfOwner = getParameterByName('owner');
+});
