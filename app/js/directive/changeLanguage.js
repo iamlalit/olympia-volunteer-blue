c539781f6a93d88114cc3639a4b54494fa18a8fe
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-11-16 17:10:33 +0530

    updated for chage language directive

--- app/js/directive/changeLanguage.js
+++ app/js/directive/changeLanguage.js
@@ -4,8 +4,10 @@ OrgVolApp.directive('changeLanguage', function($rootScope) {
      restrict: 'A',
      link: function (scope, element, attrs, ctrl) {
      	element.on('click', function(){
-            alert('here');
-            console.log($(this).closest('a'))
+            $("ul.flag li ul li").removeClass('active');
+            var source = $(this).children( "a" ).children( "img" ).attr("src");
+            $(this).parents("li.pull-right").children("#icons-header-right").children( "img" ).prop("src", source);
+            $(this).addClass('active');
      	});
      }
     };
